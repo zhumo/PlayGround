7f11462a73a0b9e8991613f4b124deae2ef712fb
mozhu <mozhu888@gmail.com>
2017-07-14 11:21:56 -0400

    fooling around with playing and pausing a song

--- app/src/main/java/com/example/android/playground/MainActivity.java
+++ app/src/main/java/com/example/android/playground/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.playground;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player = MediaPlayer.create(this, R.raw.mozart_concerto_4);
     }
+
+    public void playMusic(View view) {
+        player.start();
+    }
+
+    public void pauseMusic(View view) {
+        player.pause();
+    }7
 }
