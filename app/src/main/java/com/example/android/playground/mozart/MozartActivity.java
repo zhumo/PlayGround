45354be7c35a3b9a18c15a493afebc35ffa4fbf8
mozhu <mozhu888@gmail.com>
2017-07-15 15:27:04 -0400

    clean up mediaplayer if user leaves activity. Gracefully handle returning as well

--- app/src/main/java/com/example/android/playground/mozart/MozartActivity.java
+++ app/src/main/java/com/example/android/playground/mozart/MozartActivity.java
@@ -3,6 +3,7 @@ package com.example.android.playground.mozart;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.android.playground.R;
@@ -15,8 +16,7 @@ public class MozartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mozart);
-        player = MediaPlayer.create(this, R.raw.mozart_concerto_4);
-        player.setOnCompletionListener(new MozartOnCompletionListener(this));
+        initializePlayer();
     }
 
     public void playMusic(View view) {
@@ -25,5 +25,23 @@ public class MozartActivity extends AppCompatActivity {
 
     public void pauseMusic(View view) {
         player.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        player.release();
+        player = null;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializePlayer();
+    }
+
+    private void initializePlayer() {
+        player = MediaPlayer.create(this, R.raw.mozart_concerto_4);
+        player.setOnCompletionListener(new MozartOnCompletionListener(this));
     }
 }
